6d3f43291964595a136112405338e3f462d6e3d7
shinto.tk <110426953+shinto-tk@users.noreply.github.com>
2022-08-01 21:56:40 -0400

    Add files via upload

--- Games/AR2.lua
+++ Games/AR2.lua
@@ -309,17 +309,17 @@ local Window = Shinto.Utilities.UI:Window({
             end}):Keybind()
             MiscSection:Button({Name = "Instant Interact",Side = "Right",
             Callback = Shinto.Utilities.Misc.InstantI})
-            MiscSection:Textbox({Name = "Teleport to Player",Flag = "Misc/PlrTP",Placeholder = "Username",
-            Callback = function tp(anch)
-                pcall(function()
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = true
-                    wait(0.2)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Players")[anch].Character.HumanoidRootPart.CFrame * CFrame.new(10,5,10)
-                    wait(6)
-                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = false
-                end)
-            end
-            tp(String)})
+            --MiscSection:Textbox({Name = "Teleport to Player",Flag = "Misc/PlrTP",Placeholder = "Username",
+            --Callback = function tp(anch)
+            --    pcall(function()
+            --        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = true
+            --        wait(0.2)
+            --        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Players")[anch].Character.HumanoidRootPart.CFrame * CFrame.new(10,5,10)
+            --        wait(6)
+            --        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = false
+            --    end)
+            --end
+            --tp(String)})
         end
     end
     local SettingsTab = Window:Tab({Name = "Settings"}) do
