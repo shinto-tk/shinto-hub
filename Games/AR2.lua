fe9271e558c0aecbe929999a72c9a3a731b95d51
shinto.tk <110426953+shinto-tk@users.noreply.github.com>
2022-08-01 21:58:40 -0400

    Add files via upload

--- Games/AR2.lua
+++ Games/AR2.lua
@@ -307,19 +307,19 @@ local Window = Shinto.Utilities.UI:Window({
             MiscSection:Toggle({Name = "Map ESP",Flag = "AR2/MapESP",Value = false,Callback = function(Bool)
                 if Bool then Interface:Get("Map"):EnableGodview() else Interface:Get("Map"):DisableGodview() end
             end}):Keybind()
-            --MiscSection:Button({Name = "Instant Interact",Side = "Right",
-            --Callback = Shinto.Utilities.Misc.InstantI})
-            --MiscSection:Textbox({Name = "Teleport to Player",Flag = "Misc/PlrTP",Placeholder = "Username",
-            --Callback = function tp(anch)
-            --    pcall(function()
-            --        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = true
-            --        wait(0.2)
-            --        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Players")[anch].Character.HumanoidRootPart.CFrame * CFrame.new(10,5,10)
-            --        wait(6)
-            --        game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = false
-            --    end)
-            --end
-            --tp(String)})
+            MiscSection:Button({Name = "Instant Interact",Side = "Right",
+            Callback = Shinto.Utilities.Misc.InstantI})
+            MiscSection:Textbox({Name = "Teleport to Player",Flag = "Misc/PlrTP",Placeholder = "Username",
+            Callback = function tp(anch)
+                pcall(function()
+                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = true
+                    wait(0.2)
+                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = game:GetService("Players")[anch].Character.HumanoidRootPart.CFrame * CFrame.new(10,5,10)
+                    wait(6)
+                    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Anchored = false
+                end)
+            end
+            tp(String)})
         end
     end
     local SettingsTab = Window:Tab({Name = "Settings"}) do
